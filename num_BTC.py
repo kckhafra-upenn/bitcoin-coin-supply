a41fe4d35b03a7eba70c1ffd4e70ed92fc9ef928
kckhafra <kckhafra@gmail.com>
2021-10-18 11:08:20 -0400

    finalize

--- num_BTC.py
+++ num_BTC.py
@@ -3,10 +3,15 @@ import math
 def num_BTC(b):
     c = float(50)
     c = b*c
-    # while b>210000:
-    #     b=b/210000
-    #     c = (c/2)
-    # print(c)
+    if(b>210000):
+        d = b/210000
+        e=0
+        while(d>0):
+            e = (210000*50)/2 +e
+            d=d-1
+        print("HAlves: ",e)
+        c = e + ((b%210000)*50)
+    print(c)
     return c
 
-# num_BTC(211000)
+num_BTC(648559)
