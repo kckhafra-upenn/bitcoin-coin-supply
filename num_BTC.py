ddda2f3a6dd913ce70c9c838b516595ee689ec25
kckhafra <kckhafra@gmail.com>
2021-10-18 22:00:13 -0400

    finished last question

--- num_BTC.py
+++ num_BTC.py
@@ -3,11 +3,11 @@ import math
 def num_BTC(b):
     fl = float(50)
     c=0
-    if(b>210000):
-        results=0
-        quotient = b/210000
-        remainder = b%210000
-        multiplier = float(50)
+    results=0
+    quotient = b/210000
+    remainder = b%210000
+    multiplier = float(50)
+    if(b>210000):    
         while(quotient>0):
             results = (210000*multiplier) + results
             multiplier=multiplier/2
