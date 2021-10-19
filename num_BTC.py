4f89e018dcf8d6be80a2867449c7e08736d8736f
kckhafra <kckhafra@gmail.com>
2021-10-18 22:02:01 -0400

    finished last question

--- num_BTC.py
+++ num_BTC.py
@@ -1,14 +1,12 @@
 import math
 
 def num_BTC(b):
-    fl = float(50)
-    c=0
-    results=0
-    quotient = b/210000
-    remainder = b%210000
-    multiplier = float(50)
-    r=0
-    if(b>210000):    
+    c = float(50)
+    if(b>210000):
+        results=0
+        quotient = b/210000
+        remainder = b%210000
+        multiplier = float(50)
         while(quotient>0):
             results = (210000*multiplier) + results
             multiplier=multiplier/2
@@ -16,6 +14,7 @@ def num_BTC(b):
         r = remainder * multiplier
         c=results+r
     else:
-        c = b*fl
+        c = b*c
     return c
-# print("NUM: ",num_BTC(813574))
+
+print("NUM: ",num_BTC(813574))
